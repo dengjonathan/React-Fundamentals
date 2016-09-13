f672bdbb75b5fbb9df5ec8965d75a21b547e3089
jonathan.deng <dengjonathan@gmail.com>
2016-09-13 11:40:21 -0400

    started work of battle function, realized needed async functions

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -6,9 +6,37 @@ var sex = "YOUR_SECRET_ID";
 var param = "?client_id=" + id + "&client_secret=" + sex;
 
 function getUserInfo(username) {
-  return axios.get('https://api.github.com/users/' + username + param)
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getScore(player) {
+  var stars;
+  var followers;
+  // get player repos from gitHub API
+  getUserInfo(player) // should return promise
+  // .then get number of stars/ followers
+    .then(function(response) {
+      stars = response.followers;
+      followers = response.followers;
+    })
+    .catch(function(err) {
+      // do something on error
+    });
+}
+
+function getStars(player) {
+
+}
+
+function getFollowers(player) {
+
+}
+
+function calcScore(stars, followers) {
+
+}
+
+// TODO make github battle helpers function
 var helpers = {
   getPlayersInfo: function(players) {
     return axios.all(players.map(function(username) {
@@ -22,6 +50,12 @@ var helpers = {
       .catch(function(error) {
         console.warn("error in getPlayersInfo", error);
       });
+  },
+  battle: function(players) {
+    // return promise then do some async work
+    return axios.all(players.map(function(username) {
+      return getScore(player);
+    }))
   }
 };
 
